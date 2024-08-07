ff1f46d8bfef5a306ff9e7d6b1ca46e5178f516f
児玉源太郎 <>
2024-08-07 16:48:06 +0900

    add comamnd check

--- main_1/Core/Src/main.c
+++ main_1/Core/Src/main.c
@@ -607,6 +607,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
+	NVIC_SystemReset();
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
